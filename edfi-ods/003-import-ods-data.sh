d6d2afd09b5547ed8a506fc0dab877d2c2f50a0d
Marcos Alcozer <marcos@alcozer.dev>
2022-01-20 09:37:55 -0600

    Updates SQL import path

--- edfi-ods/003-import-ods-data.sh
+++ edfi-ods/003-import-ods-data.sh
@@ -1,9 +1,9 @@
 export PGPASSWORD=$1;
 psql -h localhost -U postgres  'EdFi_Admin' < artifacts/edfi-ods-admin/EdFi_Admin.sql;
 psql -h localhost -U postgres  'EdFi_Security' < artifacts/edfi-ods-security/EdFi_Security.sql;
-psql -h localhost -U postgres  'EdFi_Ods_2023' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.sql;
-psql -h localhost -U postgres  'EdFi_Ods_2022' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.sql;
-psql -h localhost -U postgres  'EdFi_Ods_2021' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.sql;
+psql -h localhost -U postgres  'EdFi_Ods_2023' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.TPDM.Core.sql;
+psql -h localhost -U postgres  'EdFi_Ods_2022' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.TPDM.Core.sql;
+psql -h localhost -U postgres  'EdFi_Ods_2021' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.TPDM.Core.sql;
 
 for FILE in `LANG=C ls artifacts/ed-fi-ods-admin-scripts/PgSql/* | sort -V`
     do
