4f46f9a8d61ab30e83b1f12f80f514841a6a0040
Marcos Alcozer <marcos@alcozer.dev>
2022-02-10 20:45:24 -0600

    Updates Admin App deployment command

--- edfi-admin-app/001-deploy-admin-app.sh
+++ edfi-admin-app/001-deploy-admin-app.sh
@@ -1,16 +1,20 @@
 gcloud builds submit --tag gcr.io/$1/edfi-admin-app-$3 edfi-admin-app/$3;
 
-gcloud beta run deploy edfi-admin-app-$3 \
---image gcr.io/$1/edfi-admin-app-$3 \
---add-cloudsql-instances $1:us-central1:edfi-ods-db \
---port 80 \
---region us-central1 \
---cpu 2 \
---memory 1Gi \
---concurrency 50 \
---max-instances 1 \
---allow-unauthenticated \
---update-env-vars PROJECT_ID=$1,ENCRYPTION_KEY=$(/usr/bin/openssl rand -base64 32),API_URL=$2 \
---set-secrets=DB_PASS=ods-password:1 \
---service-account edfi-cloud-run@$1.iam.gserviceaccount.com \
---platform managed;
+gcloud beta secrets add-iam-policy-binding projects/$1/secrets/admin-app-encryption-key \
+--member serviceAccount:edfi-cloud-run@$1.iam.gserviceaccount.com \
+--role roles/secretmanager.secretAccessor;
+
+gcloud beta run deploy edfi-admin-app \
+    --image gcr.io/$1/edfi-admin-app \
+    --add-cloudsql-instances $1:us-central1:edfi-ods-db \
+    --port 80 \
+    --region us-central1 \
+    --cpu 2 \
+    --memory 1Gi \
+    --concurrency 50 \
+    --max-instances 1 \
+    --allow-unauthenticated \
+    --update-env-vars PROJECT_ID=$1,API_URL=$2 \
+    --set-secrets=DB_PASS=ods-password:1,ENCRYPTION_KEY=admin-app-encryption-key:1 \
+    --service-account edfi-cloud-run@$1.iam.gserviceaccount.com \
+    --platform managed;
