f300231d503e1f152f53a62670d7aa7fc3f0fa27
Marcos Alcozer <marcos@alcozer.dev>
2022-02-10 20:26:50 -0600

    Updates ODS for TPDM Core

--- edfi-ods/002-create-cloud-sql.sh
+++ edfi-ods/002-create-cloud-sql.sh
@@ -1,10 +1,24 @@
-gcloud sql instances create \
---zone us-central1-c \
---database-version POSTGRES_11 \
---memory 7680MiB \
---cpu 2 \
---storage-auto-increase \
---backup-start-time 08:00 edfi-ods-db;
+gcloud compute addresses create google-managed-services-default \
+    --global \
+    --purpose=VPC_PEERING \
+    --prefix-length=16 \
+    --description="peering range" \
+    --network=default;
+
+gcloud services vpc-peerings connect \
+    --service=servicenetworking.googleapis.com \
+    --ranges=google-managed-services-default \
+    --network=default \
+    --project=$1;
+
+gcloud beta sql instances create \
+    --zone us-central1-c \
+    --database-version POSTGRES_11 \
+    --memory 7680MiB \
+    --cpu 2 \
+    --storage-auto-increase \
+    --network=projects/$1/global/networks/default \
+    --backup-start-time 08:00 edfi-ods-db;
 
 sleep 5;
 
--- edfi-ods/003-import-ods-data.sh
+++ edfi-ods/003-import-ods-data.sh
@@ -1,15 +1,15 @@
 export PGPASSWORD=$1;
 psql -h localhost -U postgres  'EdFi_Admin_Hogwarts' < artifacts/edfi-ods-admin/EdFi_Admin.sql;
 psql -h localhost -U postgres  'EdFi_Security_Hogwarts' < artifacts/edfi-ods-security/EdFi_Security.sql;
-psql -h localhost -U postgres  'EdFi_Ods_Hogwarts_2022' < artifacts/edfi-ods-populated/EdFi.Ods.Populated.Template.sql;
-psql -h localhost -U postgres  'EdFi_Ods_Hogwarts_2021' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.sql;
-psql -h localhost -U postgres  'EdFi_Ods_Hogwarts_2020' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.sql;
+psql -h localhost -U postgres  'EdFi_Ods_Hogwarts_2022' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.TPDM.Core.sql;
+psql -h localhost -U postgres  'EdFi_Ods_Hogwarts_2021' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.TPDM.Core.sql;
+psql -h localhost -U postgres  'EdFi_Ods_Hogwarts_2020' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.TPDM.Core.sql;
 
 psql -h localhost -U postgres  'EdFi_Admin_Ilvermorny' < artifacts/edfi-ods-admin/EdFi_Admin.sql;
 psql -h localhost -U postgres  'EdFi_Security_Ilvermorny' < artifacts/edfi-ods-security/EdFi_Security.sql;
-psql -h localhost -U postgres  'EdFi_Ods_Ilvermorny_2022' < artifacts/edfi-ods-populated/EdFi.Ods.Populated.Template.sql;
-psql -h localhost -U postgres  'EdFi_Ods_Ilvermorny_2021' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.sql;
-psql -h localhost -U postgres  'EdFi_Ods_Ilvermorny_2020' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.sql;
+psql -h localhost -U postgres  'EdFi_Ods_Ilvermorny_2022' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.TPDM.Core.sql;
+psql -h localhost -U postgres  'EdFi_Ods_Ilvermorny_2021' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.TPDM.Core.sql;
+psql -h localhost -U postgres  'EdFi_Ods_Ilvermorny_2020' < artifacts/edfi-ods-minimal/EdFi.Ods.Minimal.Template.TPDM.Core.sql;
 
 for FILE in `LANG=C ls artifacts/ed-fi-ods-admin-scripts/PgSql/* | sort -V`
     do
